3e239191e82841770b2c6222ff3058759b08a7f2
sueiz <sueiz@naver.com>
2020-10-27 18:08:26 +0900

    10/27

--- js/index.js
+++ js/index.js
@@ -1,26 +1,22 @@
 $(function() {
-
     $('.main_slide .btn').on('click',function(){
         $('.slide_wrap').css({
-            'display':'none',
-            'opacity':'0'
+            visibility:'hidden'
         })
         $('.br_wrap').css({
-            'display':'block',
-            'opacity':'1'
+            visibility:'visible'
         })
-
+        $('.slide_wrap ul').slick('slickPause');
     });
 
     $('.closeBtn').on('click',function(){
         $('.slide_wrap').css({
-            'display':'block',
-            'opacity':'1'
+            visibility:'visible'
         })
         $('.br_wrap').css({
-            'display':'none',
-            'opacity':'0'
+            visibility:'hidden'
         })
+        $('.slide_wrap ul').slick('slickPlay');
 
     })
     /* 슬라이드 */
@@ -33,6 +29,16 @@ $(function() {
         autoplaySpeed: 2000
     });
 
+    /* 애니메이션 */
+    $('.bn').eq(1).find('a').addClass('active');
+    $('.slide_wrap ul').on('beforeChange',function(event, slick, currentSlide, nextSlide){
+        $('.bn a').removeClass('active');
+    });
+
+    $('.slide_wrap ul').on('afterChange', function(event, slick, currentSlide){
+        $('.bn').eq(currentSlide+1).find('a').addClass('active');
+    });
+
     $('.br_wrap ul').slick({
         dots: true,
         infinite: true,
@@ -41,5 +47,17 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $('.event_slide ul').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        prevArrow: false,
+        nextArrow: false
+      });
+
+
 });
 
